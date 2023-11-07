8fe7fcd3cd49bed8fe21c81372042a4fdb0d5e96
iiShadow <137681594+iim7md11@users.noreply.github.com>
2023-11-07 10:04:13 +0200

    update1.0

---
+++ core/database/connection.php
@@ -0,0 +1,16 @@
+<?php
+// Not Working Go To classes/connection.php
+
+// $servername = "localhost";
+// $db_name="twitter";
+// $username = "root";
+// $password = "";
+
+// try {
+//   $pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+//   // set the PDO error mode to exception
+//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// //   echo "Connected successfully";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
